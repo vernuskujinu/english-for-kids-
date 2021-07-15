34aa2b8504ecc97a9cee443e16e808858cc5e245
vernuskujinu <vernuskujinu@gmail.com>
2021-07-15 23:17:22 +0300

    refactor: edit footer links

--- src/components/app-footer/app-footer.tsx
+++ src/components/app-footer/app-footer.tsx
@@ -6,7 +6,7 @@ const AppFooter = () => {
   return (
     <footer className="footer">
       <a href="https://github.com/vernuskujinu" className="footer__github-link">
-        My github 2021
+        vernuskujinu 2021
       </a>
       <a href="https://rs.school/" className="footer__course-link">
         RS SCHOOL
